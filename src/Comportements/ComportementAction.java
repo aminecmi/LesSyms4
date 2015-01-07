9a5c60e86ef7e388143ddf656c9a73b002906695
aminecmi <aminecmi@gmail.com>
2015-01-07 09:33:42 +0100

    Comportement pour les actions des personnages.

--- src/Comportements/ComportementAction.java
+++ src/Comportements/ComportementAction.java
@@ -1,5 +1,7 @@
 package Comportements;
 
+import Cases.CaseAbstraite;
+
 public interface ComportementAction {
-    public void executerAction();
+    public void executerAction(CaseAbstraite c);
 }
