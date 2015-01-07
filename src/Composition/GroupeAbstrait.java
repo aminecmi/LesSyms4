6a2aecc298527a91ac9fb64e976b80e55f01c449
aminecmi <aminecmi@gmail.com>
2015-01-07 11:01:08 +0100

    List de PersonnagesAbstraits dans le groupe abstrait.

--- src/Composition/GroupeAbstrait.java
+++ src/Composition/GroupeAbstrait.java
@@ -1,4 +1,11 @@
 package Composition;
 
+import java.util.ArrayList;
+
 public class GroupeAbstrait extends PersonnagesAbstraits {
+    ArrayList<PersonnagesAbstraits> list = new ArrayList<PersonnagesAbstraits>();
+
+    public GroupeAbstrait(ArrayList<PersonnagesAbstraits> list) {
+        this.list = list;
+    }
 }
